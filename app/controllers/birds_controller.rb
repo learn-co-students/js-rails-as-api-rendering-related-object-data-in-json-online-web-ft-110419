a4e14a0cc5638742face6ccd861867ca1288f8b1
LucyWeltner <lucyiweltner@gmail.com>
2020-02-27 08:34:30 -0500

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -3,4 +3,7 @@ class BirdsController < ApplicationController
     @birds = Bird.all
     render json: @birds
   end
+  def show
+  	bird = Bird.find_by_id(params[:id])
+  	render json: bird, except: [:created_at, :updated_at]
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get '/birds' => 'birds#index'
+  get '/bird/:id' => 'birds#show'
+  get '/sightings/:id' => 'sightings#show'
 end
